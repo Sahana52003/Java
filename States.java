19f79aeb1d995aa8c19121706fe7c91cb9cd090b
Sahana52003 <sahana123xworkz@gmail.com>
2025-08-10 19:58:04 -0700

    java program

---
+++ States.java
@@ -0,0 +1,27 @@
+class States
+{
+public static void main(String[] args)
+{
+int totalStates=10;
+String KarnatakaCaptial="Banglore";
+String AssamCaptial="Dispur";
+String MadhyaPradeshCaptial="Bhopal";
+String MaharastraCaptial="Mumbai";
+String HarayanaCaptial="chandigarh";
+String GoaCaptial="Panaji";
+String JharkhandCaptial="Ranchi";
+String ManipurCaptial="Impal";
+String TamilnaduCaptial="Chennai";
+String TelanganaCaptial="Hyderabad";
+System.out.println(KarnatakaCaptial);
+System.out.println(AssamCaptial);
+System.out.println( MadhyaPradeshCaptial);
+System.out.println(MaharastraCaptial);
+System.out.println(HarayanaCaptial);
+System.out.println(GoaCaptial);
+System.out.println(JharkhandCaptial);
+System.out.println(ManipurCaptial);
+System.out.println(TamilnaduCaptial);
+System.out.println(TelanganaCaptial);
+}
+}
