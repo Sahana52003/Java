e1eb4826a94b7e11ea3da4cfe908ccc42642d5a8
Sahana52003 <sahana123xworkz@gmail.com>
2025-08-11 09:22:52 -0700

    java 1 program

---
+++ Samsung.java
@@ -0,0 +1,25 @@
+class Samsung
+{
+public static void main(String[] args)
+{
+byte sizeName=8;
+short size=256;
+int size1=512;
+float valueName=456765257.879f;
+long priceName=12345678956l;
+boolean isAccount=false;
+double value=2345676545.567;
+char name='A';
+char namen=68;
+System.out.println("The size of Samsung storage capacity is : " + size);
+System.out.println("The size of Samsung storage capacity is : " + sizeName);
+System.out.println("The size of Samsung storage capacity is : " + size1);
+//System.out.println("The value of long is : " + priceName );
+System.out.println("The value of float is : " + valueName);
+System.out.println("The value of double is : " + value);
+System.out.println("The boolean of isAccount is : " + isAccount);
+System.out.println("The name is : " + name);
+System.out.println("The name is : " + namen);
+
+}
+}
