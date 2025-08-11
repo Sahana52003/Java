9399d4a41b0e7bfe06436fe92cc3debeaec1eefa
Sahana52003 <sahana123xworkz@gmail.com>
2025-08-11 09:40:29 -0700

    Object References

---
+++ TVSpecs.java
@@ -0,0 +1,27 @@
+class TVSpecs {
+    public static void main(String[] args) {
+        
+        // Non-primitive data types (Reference Types)
+        String screenSize = "43 Inches";
+        String brand = "Samsung";
+        String resolution = "4K";
+        String refreshRate = "50 Hz";
+        String specialFeature = "Dynamic Crystal Color | Solar Cell Remote Controller | AirSlim Design | "
+                + "In-Built Alexa & Bixby (Remote Control Voice Assistants) | 4K Upscaling | Endless Content";
+        String includedComponents = "1 Number LED TV, 1 Number Power Cord, 2 Numbers Stand P - Cover Top, 1 Number Remote Control";
+        String connectivityTechnology = "Wi-Fi";
+        String aspectRatio = "16:9";
+        String productDimensions = "2.6D x 96.6W x 56H Centimeters";
+        String supportedInternetServices = "Netflix";
+        System.out.println("Screen Size of TVSpecs is : " + screenSize);
+        System.out.println("Brand of TVSpecs is : " + brand);
+        System.out.println("Resolution of TVSpecs is : " + resolution);
+        System.out.println("Refresh Rate of TVSpecs is : " + refreshRate);
+        System.out.println("Special Feature of TVSpecs is : " + specialFeature);
+        System.out.println("Included Components of TVSpecs is : " + includedComponents );
+        System.out.println("Connectivity Technology of TVSpecs is : " + connectivityTechnology );
+        System.out.println("Aspect Ratio of TVSpecs is : " + aspectRatio);
+        System.out.println("Product Dimensions of TVSpecs is : " + productDimensions);
+        System.out.println("Supported Internet Services of TVSpecs is : " + supportedInternetServices);
+    }
+}
