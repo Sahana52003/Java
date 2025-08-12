77910ea333eb7dd5439e73615467f66a1875493f
Sahana52003 <sahana123xworkz@gmail.com>
2025-08-12 17:22:33 +0530

    Variables and Reassign the values

---
+++ Dell.java
@@ -0,0 +1,36 @@
+class Dell
+{
+public static void main(String[] args)
+{
+String Brand="Dell";
+Brand ="Dell 15";
+byte size =8;
+size=82;
+short sizeV=33;
+sizeV=256;
+int valueName=17548;
+valueName=2674;
+long size1=76545765766l;
+size1=7984576354l;
+float sizeName=876545677.33f;
+sizeName=32455455678.22f;
+double price=875457457.86;
+price=34542445.64;
+char names='V';
+names='R';
+char nameV=55;
+nameV=35;
+Boolean isQuality=false;
+isQuality=true;
+System.out.println("The brand of Acer is : " + Brand);
+System.out.println("The byte size of brand is : " + size);
+System.out.println("The sizeV of brand is : " + sizeV);
+System.out.println("The valueName of brand is : " + valueName);
+System.out.println("The size1 of  brand is : " + size1);
+System.out.println("The sizeName of  brand is : " + sizeName);
+System.out.println("The price of  brand is : " + price);
+System.out.println("The names of  brand is : " + names);
+System.out.println("The nameV of  brand is : " + nameV);
+System.out.println("The brand is  isQuality : " + isQuality);
+}
+}
---
+++ MSI.java
@@ -0,0 +1,35 @@
+class MSI{
+public static void main(String[] args)
+{
+String Brand="MSI";
+Brand ="MSI Modern 14";
+byte size =4;
+size=124;
+short sizeV=376;
+sizeV=3426;
+int valueName=36678;
+valueName=2465;
+long size1=14547904567l;
+size1=57763434545l;
+float sizeName=866546754.2343f;
+sizeName=573456765.2232f;
+double price=253248744.87546;
+price=47234565562.234;
+char names='Z';
+names='T';
+char nameV=86;
+nameV=45;
+Boolean isValid=true;
+isValid=false;
+System.out.println("The brand of HP is : " + Brand);
+System.out.println("The byte size of brand is : " + size);
+System.out.println("The sizeV of brand is : " + sizeV);
+System.out.println("The valueName of brand is : " + valueName);
+System.out.println("The size1 of  brand is : " + size1);
+System.out.println("The sizeName of  brand is : " + sizeName);
+System.out.println("The price of  brand is : " + price);
+System.out.println("The names of  brand is : " + names);
+System.out.println("The nameV of  brand is : " + nameV);
+System.out.println("The brand is  isvalid : " + isValid);
+}
+}
