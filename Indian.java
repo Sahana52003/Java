68b8d93cea690b1c4042840a3828f38f2a7eb142
Sahana52003 <sahana123xworkz@gmail.com>
2025-08-13 19:11:44 +0530

    Array

---
+++ Indian.java
@@ -0,0 +1,29 @@
+class Indian
+{
+    public static void main(String[] args)
+ {
+        String[] dishes = {
+            "Chapati",
+            "Dosa",
+            "Biryani",
+            "Paneer Butter Masala",
+            "Idli",
+            "Samosa",
+            "Aloo Paratha",
+            "Pav Bhaji",
+            "Chole Bhature",
+            "Tomato Rice"
+        };
+System.out.println("The Indian Dishes are : " + dishes[0]);
+System.out.println("The Indian Dishes are : " + dishes[1]);
+System.out.println("The Indian Dishes are : " + dishes[2]);
+System.out.println("The Indian Dishes are : " + dishes[3]);
+System.out.println("The Indian Dishes are : " + dishes[4]);
+System.out.println("The Indian Dishes are : " + dishes[5]);
+System.out.println("The Indian Dishes are : " + dishes[6]);
+System.out.println("The Indian Dishes are : " + dishes[7]);
+System.out.println("The Indian Dishes are : " + dishes[8]);
+System.out.println("The Indian Dishes are : " + dishes[9]);
+System.out.println("The Indian Dishes are : " + dishes.length);
+}
+}
---
+++ TomatoRice.java
@@ -0,0 +1,29 @@
+class TomatoRice
+{
+public static void main(String[] args)
+{
+String[] ingredients = {
+            "Garam Masala",
+            "Green Chili",
+            "Tomatos",
+            "Ginger",
+            "Cloves",
+            "BayLeaf",
+            "Cinnamon",
+            "Cardamoms",
+            "Onions",
+            "Turmeric"
+        };
+System.out.println("The ingredients for TomatoRice are : " + ingredients[0]);
+System.out.println("The ingredients for TomatoRice are : " + ingredients[1]);
+System.out.println("The ingredients for TomatoRice are : " + ingredients[2]);
+System.out.println("The ingredients for TomatoRice are : " + ingredients[3]);
+System.out.println("The ingredients for TomatoRice are : " + ingredients[4]);
+System.out.println("The ingredients for TomatoRice are : " + ingredients[5]);
+System.out.println("The ingredients for TomatoRice are : " + ingredients[6]);
+System.out.println("The ingredients for TomatoRice are : " + ingredients[7]);
+System.out.println("The ingredients for TomatoRice are : " + ingredients[8]);
+System.out.println("The ingredients for TomatoRice are : " + ingredients[9]);
+System.out.println("The ingredients for TomatoRice are : " + ingredients.length);
+}
+}
