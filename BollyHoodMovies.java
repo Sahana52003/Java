9de582c6909a20b7b386f88851d628eb2ebe173a
Sahana52003 <sahana123xworkz@gmail.com>
2025-08-18 20:02:49 +0530

    Arrays using for,if,if else, for loop

--- BollyHoodMovies.java
+++ BollyHoodMovies.java
@@ -3,14 +3,28 @@ class BollyHoodMovies {
         String[] movieNames = {
             "Sholay", "Dilwale Dulhania Le Jayenge", "3 Idiots", "PK",
             "Lagaan", "Zindagi Na Milegi Dobara", "Dangal",
-            "Gully Boy", "Chak De! India", "Kabir Singh"};
-        System.out.println("Bollywood Movies :");
+            "Gully Boy", "Chak De! India", "Kabir Singh"
+        };
+
+        System.out.println("Bollywood Movies (using for loop):");
         for (int i = 0; i < movieNames.length; i++) {
-            System.out.println("Movie Name: " + movieNames[i]);
+            if (i < 10) {  // valid check
+                System.out.println("Movie Name: " + movieNames[i]);
+                continue;
+            }
         }
+
         System.out.println("======================");
+
+        System.out.println("Bollywood Movies (using foreach loop):");
         for (String movie : movieNames) {
             System.out.println("Movie Name: " + movie);
+        }
+
+        // Example: check if array has at least 6 movies
+        if (movieNames.length >= 6) {
+            System.out.println("======================");
+            System.out.println("Yes, there are " + movieNames.length + " movies in the list.");
         }
     }
 }
