bb63be343a58de6dbb3e74ff04d7ce1f79856ee4
Sahana52003 <sahana123xworkz@gmail.com>
2025-08-18 20:14:39 +0530

    arrays using for,if,if else,for each loop

--- KannadaMovies.java
+++ KannadaMovies.java
@@ -4,13 +4,25 @@ class KannadaMovies {
             "KGF", "Kantara", "777Charlie", "Rajakumara", "Ugram",
             "Yuvarathnaa", "Bhajarangi", "Lucia", "KGF-2", "Kurukshetra"
         };
+       String[] movie={ "Yuvarathnaa", "Bhajarangi", "Lucia", "KGF-2", "Kurukshetra"};
         System.out.println("Kannada Movies:");
         for (int i = 0; i < movieNames.length; i++) {
-            System.out.println("Movie Name: " + movieNames[i]);
+            if (i<5) {
+                System.out.println("Movie Name :" + movieNames[i]);
+ 		continue;
+            }
         }
-	System.out.println("==============");
-        for (String movie : movieNames) {
-            System.out.println("Kannada Movie: " + movie);
+        System.out.println("======================");
+        for (String moviee : movie) {
+            System.out.println("Movie Name: " + movie);
         }
+	if (movie.length <10) {
+            System.out.println("======================");
+            System.out.println("There are less than 6 movies in the list. : " + movie.length);
+        } else {
+            System.out.println("======================");
+            System.out.println("There are " + movie.length + " movies in the list.");
+}
     }
+
 }
