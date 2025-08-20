c2787bbdcd7f3945d04a914f5fe67d4638a6e400
Sahana52003 <sahana123xworkz@gmail.com>
2025-08-20 23:24:57 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,3 +1,47 @@
+class PrimeNumber
+{
+public static void main(String[] args)
+{
+int n=6;
+if(n%2==0){
+System.out.println("Yes, it is a Prime Number");
+}else{
+System.out.println("It is not a PrimeNumber");
+}
+}
+}
+
+
+
+class Prime {
+    public static void main(String[] args) {
+        int[] primeValues = {2,4,6,11,14,23,28,31,36,45,67,73};
+        for (int n : primeValues) {
+            if (n <= 1) {
+                System.out.println(" is  Prime number:" + n);
+            } else {
+		int j;
+                for ( j = 2; j < n; j++) {
+                    if (n % j == 0) {
+			System.out.println(" is  Prime number:" + n);
+                        break; 
+                    }
+                }
+		
+                if (j == n) {
+                    System.out.println(n + " is a Prime number.");
+                } else {
+                    System.out.println(n + " is NOT a Prime number.");
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
 class Prime {
     public static void main(String[] args) {
         int[] primeValues = {3,6,1,12,11,45,17,29,31,38,43,47,19};
