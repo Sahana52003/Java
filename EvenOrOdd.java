d97c4d12abdbac9fe7cd44af6400dcd19c713921
Sahana52003 <sahana123xworkz@gmail.com>
2025-08-20 23:21:44 +0530

    Prime,Even or odd,Factorial

---
+++ EvenOrOdd.java
@@ -0,0 +1,36 @@
+class EvenOrOdd {
+    public static void main(String[] args) {
+        int n = 10; 
+        if (n % 2 == 0) {
+            System.out.println("Yes It is Even :" + n);
+        } else {
+            System.out.println("It is Odd : " + n);
+        }
+    }
+}
+
+
+
+class EvenOrOdd {
+    public static void main(String[] args) {
+        int[] numbers = {2, 5, 8, 11, 14, 17};
+	System.out.println("Checking whether the number is Even or Odd");
+      	 for(int n:numbers){
+            if ( n%2==0) {
+                System.out.println("Yes,The number is Even :" + n);
+            } else {
+                System.out.println("The Number is Odd : " + n);
+            }
+		System.out.println("=============================");
+            switch (n%2) {
+                case 0:
+                    System.out.println("The number is Even : " + n);
+                    break;
+                case 1:
+                    System.out.println("The number is Odd : " + n);
+                    break;
+            }
+        }
+    }
+}
+
---
+++ Factorial.java
@@ -0,0 +1,11 @@
+class Factorial {
+    public static void main(String[] args) {
+         int n = 5;            
+         int fact = n; 
+	for (int i = n-1; i>=1; i--) {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of " + n + " is : " + fact);
+    }
+}  
+             
