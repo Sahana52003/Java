480ae1b429d85fd3ae4630eaeca6675370e03843
Sahana52003 <sahana123xworkz@gmail.com>
2025-08-21 10:03:14 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -3,7 +3,8 @@ class PrimeNumber
 public static void main(String[] args)
 {
 int n=6;
-if(n%2==0){
+	int i=2;
+if(n%i==0){
 System.out.println("Yes, it is a Prime Number");
 }else{
 System.out.println("It is not a PrimeNumber");
