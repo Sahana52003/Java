a9f33c705aeea3ae32ef7bf123d5c7ee0990d443
Sahana52003 <sahana123xworkz@gmail.com>
2025-08-22 18:59:05 +0530

    USING STATIC VARIABLES

---
+++ Languages.java
@@ -0,0 +1,31 @@
+class Languages {
+    static String title = "Telugu";
+    static double price = 499.99;
+    static int prices = 200;
+    static long number = 3256778787878L;
+    static String[] value = {"Kannada", "English", "Hindi", "Telugu"};
+    static char x = 57; 
+    static boolean isQuality = true;
+    static double num = 465748.645;
+    public static void main(String[] args) 
+{
+      title="Spanish";
+     price=255.88;  
+    prices=647;
+      number=908463546L;
+      x=70;
+      isQuality=false;
+     num=1234567.865667;
+	value[3]="French";
+     System.out.println("The title is : " + title);
+       System.out.println("Price : " + price);
+      System.out.println("Prices : " + prices);
+       System.out.println("Number : " + number);
+        System.out.println("X : " + x);
+        System.out.println("isQuality : " + isQuality);
+        System.out.println("Num : " + num);
+    System.out.println("value : " + value[3]);
+		
+        
+    }
+}
