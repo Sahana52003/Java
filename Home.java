84706b1c2180acc2c18d37509d22e525f8b68aaa
Sahana52003 <sahana123xworkz@gmail.com>
2025-09-02 18:50:44 +0530

    Using Static Methods to print

---
+++ Home.java
@@ -0,0 +1,25 @@
+class Home
+{
+public static void wakeUp() {
+System.out.println("Woke up and got ready for the day");
+}
+public static void havingBreakfast() {
+System.out.println("Had a breakfast with spicy");
+}
+public static void movie() {
+System.out.println("Watching my favorite movies");
+}
+public static void study() {
+System.out.println("Started studying");
+}
+public static void relaxTime() {
+System.out.println("Listening my Favorite songs in relax time");
+}
+public static void main(String[] args) {
+wakeUp();
+havingBreakfast();
+movie();
+study();
+relaxTime();
+    }
+}
