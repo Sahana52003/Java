053ad9c061e6e1f6ddc81b0a0b7232d9fb67d0e0
Sahana52003 <sahana123xworkz@gmail.com>
2025-09-09 13:06:53 +0530

    returns a value of different data types

---
+++ Example1.java
@@ -0,0 +1,44 @@
+class  Example1{
+    public static byte number() {
+	System.out.println("The Number is .........");
+        return 67;
+    }
+    public static short value() {
+	System.out.println("The value is-------");
+        return 112;
+    }
+    public static int sum() {
+	System.out.println("The Calculated number is ");
+        return 56+12;
+    }
+    public static long phoneNumber() {
+	System.out.println("The phoneNumber is");
+        return 156789789L;
+    }
+    public static float average() {
+	System.out.println("The Average is ");
+        return 17.67f;
+    }
+    public static double pivalue() {
+	System.out.println("The Pivalue is ");
+        return 3.14;
+    }
+    public static char letter() {
+	System.out.println("The letter is ");
+        return 'Z';
+    }
+    public static boolean isAvaliable() {
+	System.out.println("Is is avaliable");
+        return true;
+    }
+    public static String name() {
+	System.out.println("The person name");
+        return "SORA"; 
+}
+    public static String[] brandNames() {
+        String[] brands = {"Gucci","Zara","H&M","CK"}; 
+	System.out.println("The Brands ");
+        return brands; 
+}
+
+}
---
+++ Example3.java
@@ -0,0 +1,44 @@
+class  Example3{
+    public static byte number1() {
+	System.out.println("The Number is .........");
+        return 107;
+    }
+    public static short value2() {
+	System.out.println("The value is-------");
+        return 2000;
+    }
+    public static int addition() {
+	System.out.println("The Calculated number is ");
+        return 77+777;
+    }
+    public static long population() {
+	System.out.println("The phoneNumber is");
+        return 157897898L;
+    }
+    public static float averages() {
+	System.out.println("The Average is ");
+        return 45.65787f;
+    }
+    public static double avgs() {
+	System.out.println("The avgs is ");
+        return 7879.43567;
+    }
+    public static char grade() {
+	System.out.println("The Grade is ");
+        return 'C';
+    }
+    public static boolean isPositive() {
+	System.out.println("Is is Positive");
+        return false;
+    }
+    public static String course() {
+	System.out.println("The course name");
+        return "JAVA";
+    } 
+     public static boolean[] isVowels() {
+        boolean letters[] = {true,false,true,false,false}; 
+	System.out.println("The Vowels ");
+        return letters; 
+}
+
+}
