2cf41fe2c58b3f25476a3a6a1f31dcb969b07f14
Sahana52003 <sahana123xworkz@gmail.com>
2025-09-24 23:43:19 +0530

    Same method name but different in arguments

---
+++ LinkedIn.java
@@ -0,0 +1,13 @@
+class LinkedIn 
+{
+public static void applyJob(String title) {
+System.out.println("Job title is " + title);
+}
+public static void applyJob(String post,int shares) {
+System.out.println("Posted Jobs " + post + " And shared " + shares);
+}
+public static boolean applyJob(String skills,long phoneNumber) {
+System.out.println("The required skill " + skills + " and number " + phoneNumber);
+return true;
+}
+}
