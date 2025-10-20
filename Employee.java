0f16f62710c2f16ae1dac181f35694b3c5ae67d7
Sahana52003 <sahana123xworkz@gmail.com>
2025-10-20 21:57:28 +0530

    parent class->child classes

--- Employee.java
+++ Employee.java
@@ -1,29 +1,5 @@
 class Employee {
-static String name="Rahul";
-static int id=1024;
-static double salary=55000.75;
-static String department="IT";
-static boolean isPermanent=true;
-public static void personName() {
-System.out.println("Employee Name: " + name);
-personNumber();
-}
-public static void personNumber() {
-System.out.println("Employee number ID is : " + id);
-personSalary();
-}
-public static void personSalary(){
-System.out.println("Employee Salary in the company is : " + salary);
-working();
-}
-public static void working() {
-System.out.println("Employee working in the area is : " + department);
-personStatus();
-}
-public static void personStatus() {
-System.out.println("Employee Staus is : " + isPermanent);
-}
-public static void main(String[] args) {
-personName(); 
+   public void entered() {
+ System.out.println("Entering to office at ."); 
 }
 }
--- Student.java
+++ Student.java
@@ -1,11 +1,5 @@
-class Student
-{
-public static void main(String[] args)
-{
-System.out.println("Name :Sahana N");
-System.out.println("email:sn68698@gmail.com");
-System.out.println("Address :Kolar");
-System.out.println("College Name :Sri Siddhartha Institute of Technology");
-System.out.println("College Address : Tumkur");
+class Student extends Person {
+   public void study() {
+ System.out.println("Studying for exams"); 
 }
 }
